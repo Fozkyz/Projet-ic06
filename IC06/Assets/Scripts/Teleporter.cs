b2e5bb42c46244c9556f32adfe1fcd91d04393cd
Fozkyz <fozkyz@gmail.com>
2022-10-06 16:13:07 +0200

    Added teleporter class + a few basic rooms

---
+++ IC06/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+	[SerializeField] private float cooldownTime = 1f;
+
+	private Teleporter linkedTeleporter;
+	private float timeSinceUsed;
+
+	public void SetLinkedTeleporter(Teleporter tp)
+	{
+		linkedTeleporter = tp;
+	}
+
+	public void TeleportPlayer(PlayerController player)
+	{
+		player.transform.position = transform.position;
+		timeSinceUsed = 0f;
+	}
+
+	private void Update()
+	{
+		timeSinceUsed += Time.deltaTime;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (timeSinceUsed > cooldownTime)
+		{
+			PlayerController player = collision.GetComponent<PlayerController>();
+			if (player != null)
+			{
+				linkedTeleporter.TeleportPlayer(player);
+			}
+		}
+	}
+}
---
+++ IC06/Assets/Scripts/TeleporterManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterManager : MonoBehaviour
+{
+	[SerializeField] private Teleporter leftTeleporter;
+	[SerializeField] private Teleporter rightTeleporter;
+	[SerializeField] private Teleporter topTeleporter;
+	[SerializeField] private Teleporter bottomTeleporter;
+
+}
